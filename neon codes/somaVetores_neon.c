9134edee9f6344b5a1b0d7d042201d18cabdb6a2
Davi Hermógenes <paciencia.2001@gmail.com>
2023-08-04 18:54:01 -0300

    Update somaVetores_neon.c

--- neon codes/somaVetores_neon.c
+++ neon codes/somaVetores_neon.c
@@ -2,16 +2,8 @@
 #include <arm_neon.h>
 //Programa NEON que soma dois vetores
 
-/*A função vld1q_f32 é usada para carregar quatro elementos float (128 bits) 
-de um endereço de memória contíguo e armazená-los em um único registrador SIMD chamado 
-float32x4_t. Isso permite o carregamento paralelo de quatro elementos float em um único 
-passo, o que pode melhorar significativamente o desempenho em operações com vetores grandes.*/
-
-/*A função vst1q_f32 é usada para armazenar quatro elementos float (128 bits) de um 
-registrador SIMD float32x4_t em um endereço de memória contíguo. Ela é usada para armazenar 
-o resultado de operações SIMD de volta à memória principal.*/
 int main() {
-    //...
+    //Funcionando
     float a[4] = {1.0, 2.0, 3.0, 4.0};
     float a2[4] = {5.0, 6.0, 7.0, 8.0};
     float b[4] = {8.0, 7.0, 6.0, 5.0};
