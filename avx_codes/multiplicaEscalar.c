d6fdbe1067058c73b7f751f833b9cae34c4ef83d
Davi Hermógenes <paciencia.2001@gmail.com>
2023-08-04 18:51:49 -0300

    Update multiplicaEscalar.c

--- avx_codes/multiplicaEscalar.c
+++ avx_codes/multiplicaEscalar.c
@@ -3,11 +3,10 @@
 
 int main() {
     float a[8] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
-    float scalar = 2.0;
+    float escalar = 5.0;
     float result[8];
-
     __m256 avx_a = _mm256_loadu_ps(a);
-    __m256 avx_scalar = _mm256_set1_ps(scalar);
+    __m256 avx_scalar = _mm256_set1_ps(escalar);
 
     // Multiplica cada elemento do vetor por um escalar
     __m256 avx_result = _mm256_mul_ps(avx_a, avx_scalar);
